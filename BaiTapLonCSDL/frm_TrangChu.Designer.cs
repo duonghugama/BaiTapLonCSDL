3f00b7ad5a371358c5e1bd116a58da38c45fa3ba
tranvantuann021201 <tvtuan0212@gmail.com>
2021-10-26 17:37:59 +0700

    frm_Muon

--- BaiTapLonCSDL/frm_TrangChu.Designer.cs
+++ BaiTapLonCSDL/frm_TrangChu.Designer.cs
@@ -52,7 +52,7 @@ namespace BaiTapLonCSDL
             this.quanLyToolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1001, 24);
             this.menuStrip1.TabIndex = 9;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,27 +63,27 @@ namespace BaiTapLonCSDL
             this.mbi_DangXuat,
             this.mbi_Exit});
             this.quanLyToolStripMenuItem.Name = "quanLyToolStripMenuItem";
-            this.quanLyToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.quanLyToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
             this.quanLyToolStripMenuItem.Text = "Tài khoản";
             // 
             // mbi_ThongTinTaiKhoan
             // 
             this.mbi_ThongTinTaiKhoan.Name = "mbi_ThongTinTaiKhoan";
-            this.mbi_ThongTinTaiKhoan.Size = new System.Drawing.Size(177, 22);
+            this.mbi_ThongTinTaiKhoan.Size = new System.Drawing.Size(178, 22);
             this.mbi_ThongTinTaiKhoan.Text = "Thông tin tài khoản";
             this.mbi_ThongTinTaiKhoan.Click += new System.EventHandler(this.mbi_ThongTinTaiKhoan_Click);
             // 
             // mbi_DangXuat
             // 
             this.mbi_DangXuat.Name = "mbi_DangXuat";
-            this.mbi_DangXuat.Size = new System.Drawing.Size(177, 22);
+            this.mbi_DangXuat.Size = new System.Drawing.Size(178, 22);
             this.mbi_DangXuat.Text = "Đăng xuất";
             this.mbi_DangXuat.Click += new System.EventHandler(this.mbi_DangXuat_Click);
             // 
             // mbi_Exit
             // 
             this.mbi_Exit.Name = "mbi_Exit";
-            this.mbi_Exit.Size = new System.Drawing.Size(177, 22);
+            this.mbi_Exit.Size = new System.Drawing.Size(178, 22);
             this.mbi_Exit.Text = "Thoát";
             this.mbi_Exit.Click += new System.EventHandler(this.mbi_Exit_Click);
             // 
@@ -102,35 +102,35 @@ namespace BaiTapLonCSDL
             // mbi_Muon
             // 
             this.mbi_Muon.Name = "mbi_Muon";
-            this.mbi_Muon.Size = new System.Drawing.Size(180, 22);
+            this.mbi_Muon.Size = new System.Drawing.Size(170, 22);
             this.mbi_Muon.Text = "Mượn ";
             this.mbi_Muon.Click += new System.EventHandler(this.mbi_Muon_Click);
             // 
             // mbi_Tra
             // 
             this.mbi_Tra.Name = "mbi_Tra";
-            this.mbi_Tra.Size = new System.Drawing.Size(180, 22);
+            this.mbi_Tra.Size = new System.Drawing.Size(170, 22);
             this.mbi_Tra.Text = "Trả";
             this.mbi_Tra.Click += new System.EventHandler(this.mbi_Tra_Click);
             // 
             // mbi_QuanLyNhanVien
             // 
             this.mbi_QuanLyNhanVien.Name = "mbi_QuanLyNhanVien";
-            this.mbi_QuanLyNhanVien.Size = new System.Drawing.Size(180, 22);
+            this.mbi_QuanLyNhanVien.Size = new System.Drawing.Size(170, 22);
             this.mbi_QuanLyNhanVien.Text = "Quản lý nhân viên";
             this.mbi_QuanLyNhanVien.Click += new System.EventHandler(this.mbi_QuanLyNhanVien_Click);
             // 
             // mbi_QuanLyDocGia
             // 
             this.mbi_QuanLyDocGia.Name = "mbi_QuanLyDocGia";
-            this.mbi_QuanLyDocGia.Size = new System.Drawing.Size(180, 22);
+            this.mbi_QuanLyDocGia.Size = new System.Drawing.Size(170, 22);
             this.mbi_QuanLyDocGia.Text = "Quản lý độc giả";
             this.mbi_QuanLyDocGia.Click += new System.EventHandler(this.mbi_QuanLyDocGia_Click);
             // 
             // mbi_QuanLySach
             // 
             this.mbi_QuanLySach.Name = "mbi_QuanLySach";
-            this.mbi_QuanLySach.Size = new System.Drawing.Size(180, 22);
+            this.mbi_QuanLySach.Size = new System.Drawing.Size(170, 22);
             this.mbi_QuanLySach.Text = "Quản lý sách";
             this.mbi_QuanLySach.Click += new System.EventHandler(this.mbi_QuanLySach_Click);
             // 
@@ -143,7 +143,7 @@ namespace BaiTapLonCSDL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.ClientSize = new System.Drawing.Size(1001, 378);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frm_TrangChu";
