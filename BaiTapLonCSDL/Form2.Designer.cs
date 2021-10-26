5997cdfdfdeb0cfb6b8e7f43e3b29ba6f3006101
tranvantuann021201 <tvtuan0212@gmail.com>
2021-10-26 13:51:21 +0700

    frm_TrangChu

---
+++ BaiTapLonCSDL/Form2.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace BaiTapLonCSDL
+{
+    partial class frm_TrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabCtrl_TrangChu = new System.Windows.Forms.TabControl();
+            this.tabPg_MuonSach = new System.Windows.Forms.TabPage();
+            this.tabPg_TraSach = new System.Windows.Forms.TabPage();
+            this.tabPg_QuanLyDocGia = new System.Windows.Forms.TabPage();
+            this.tabPg_QuanLySach = new System.Windows.Forms.TabPage();
+            this.tabPg_QuanLyNhanVien = new System.Windows.Forms.TabPage();
+            this.txt_TimKiem = new System.Windows.Forms.TextBox();
+            this.btn_TimKiem = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabCtrl_TrangChu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabCtrl_TrangChu
+            // 
+            this.tabCtrl_TrangChu.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabCtrl_TrangChu.Controls.Add(this.tabPg_MuonSach);
+            this.tabCtrl_TrangChu.Controls.Add(this.tabPg_TraSach);
+            this.tabCtrl_TrangChu.Controls.Add(this.tabPg_QuanLyDocGia);
+            this.tabCtrl_TrangChu.Controls.Add(this.tabPg_QuanLySach);
+            this.tabCtrl_TrangChu.Controls.Add(this.tabPg_QuanLyNhanVien);
+            this.tabCtrl_TrangChu.Location = new System.Drawing.Point(12, 118);
+            this.tabCtrl_TrangChu.Name = "tabCtrl_TrangChu";
+            this.tabCtrl_TrangChu.SelectedIndex = 0;
+            this.tabCtrl_TrangChu.Size = new System.Drawing.Size(776, 403);
+            this.tabCtrl_TrangChu.TabIndex = 0;
+            // 
+            // tabPg_MuonSach
+            // 
+            this.tabPg_MuonSach.Location = new System.Drawing.Point(4, 24);
+            this.tabPg_MuonSach.Name = "tabPg_MuonSach";
+            this.tabPg_MuonSach.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPg_MuonSach.Size = new System.Drawing.Size(768, 375);
+            this.tabPg_MuonSach.TabIndex = 0;
+            this.tabPg_MuonSach.Text = "MƯỢN SÁCH";
+            this.tabPg_MuonSach.UseVisualStyleBackColor = true;
+            // 
+            // tabPg_TraSach
+            // 
+            this.tabPg_TraSach.Location = new System.Drawing.Point(4, 24);
+            this.tabPg_TraSach.Name = "tabPg_TraSach";
+            this.tabPg_TraSach.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPg_TraSach.Size = new System.Drawing.Size(768, 375);
+            this.tabPg_TraSach.TabIndex = 1;
+            this.tabPg_TraSach.Text = "TRẢ SÁCH";
+            this.tabPg_TraSach.UseVisualStyleBackColor = true;
+            // 
+            // tabPg_QuanLyDocGia
+            // 
+            this.tabPg_QuanLyDocGia.Location = new System.Drawing.Point(4, 24);
+            this.tabPg_QuanLyDocGia.Name = "tabPg_QuanLyDocGia";
+            this.tabPg_QuanLyDocGia.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPg_QuanLyDocGia.Size = new System.Drawing.Size(768, 375);
+            this.tabPg_QuanLyDocGia.TabIndex = 2;
+            this.tabPg_QuanLyDocGia.Text = "QUẢN LÝ ĐỘC GIẢ";
+            this.tabPg_QuanLyDocGia.UseVisualStyleBackColor = true;
+            // 
+            // tabPg_QuanLySach
+            // 
+            this.tabPg_QuanLySach.Location = new System.Drawing.Point(4, 24);
+            this.tabPg_QuanLySach.Name = "tabPg_QuanLySach";
+            this.tabPg_QuanLySach.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPg_QuanLySach.Size = new System.Drawing.Size(768, 375);
+            this.tabPg_QuanLySach.TabIndex = 3;
+            this.tabPg_QuanLySach.Text = "QUẢN LÝ SÁCH";
+            this.tabPg_QuanLySach.UseVisualStyleBackColor = true;
+            // 
+            // tabPg_QuanLyNhanVien
+            // 
+            this.tabPg_QuanLyNhanVien.Location = new System.Drawing.Point(4, 24);
+            this.tabPg_QuanLyNhanVien.Name = "tabPg_QuanLyNhanVien";
+            this.tabPg_QuanLyNhanVien.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPg_QuanLyNhanVien.Size = new System.Drawing.Size(768, 375);
+            this.tabPg_QuanLyNhanVien.TabIndex = 4;
+            this.tabPg_QuanLyNhanVien.Text = "QUẢN LÝ NHÂN VIÊN";
+            this.tabPg_QuanLyNhanVien.UseVisualStyleBackColor = true;
+            // 
+            // txt_TimKiem
+            // 
+            this.txt_TimKiem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_TimKiem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_TimKiem.Location = new System.Drawing.Point(434, 27);
+            this.txt_TimKiem.Name = "txt_TimKiem";
+            this.txt_TimKiem.Size = new System.Drawing.Size(183, 29);
+            this.txt_TimKiem.TabIndex = 1;
+            // 
+            // btn_TimKiem
+            // 
+            this.btn_TimKiem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_TimKiem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_TimKiem.Location = new System.Drawing.Point(623, 27);
+            this.btn_TimKiem.Name = "btn_TimKiem";
+            this.btn_TimKiem.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_TimKiem.Size = new System.Drawing.Size(86, 29);
+            this.btn_TimKiem.TabIndex = 2;
+            this.btn_TimKiem.Text = "Tìm kiếm";
+            this.btn_TimKiem.UseVisualStyleBackColor = true;
+            this.btn_TimKiem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(347, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tìm kiếm: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "https://upload.wikimedia.org/wikipedia/vi/b/bc/Logo-Thuy_Loi.png";
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Banner", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(148, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(380, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Thư Viện Trường ĐH THỦY LỢI";
+            // 
+            // frm_TrangChu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_TimKiem);
+            this.Controls.Add(this.txt_TimKiem);
+            this.Controls.Add(this.tabCtrl_TrangChu);
+            this.Name = "frm_TrangChu";
+            this.Text = "Trang Chủ";
+            this.tabCtrl_TrangChu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabCtrl_TrangChu;
+        private System.Windows.Forms.TabPage tabPg_MuonSach;
+        private System.Windows.Forms.TabPage tabPg_TraSach;
+        private System.Windows.Forms.TabPage tabPg_QuanLyDocGia;
+        private System.Windows.Forms.TabPage tabPg_QuanLySach;
+        private System.Windows.Forms.TabPage tabPg_QuanLyNhanVien;
+        private System.Windows.Forms.TextBox txt_TimKiem;
+        private System.Windows.Forms.Button btn_TimKiem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
--- BaiTapLonCSDL/Program.cs
+++ BaiTapLonCSDL/Program.cs
@@ -17,7 +17,7 @@ namespace BaiTapLonCSDL
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frm_TrangChu());
         }
     }
 }
